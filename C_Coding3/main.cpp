25fa972b4fd29cc0bf8f80c0a674e415d0109ffe
xingzhili2007 <xingzhili2007@163.com>
2021-03-06 15:14:51 +0800

    Initial Commit

---
+++ C_Coding3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C_Coding3
+//
+//  Created by 李行之 on 2021/3/6.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
